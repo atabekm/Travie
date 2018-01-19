8890014f69129726dfd02ce75b8b6f18c2b8d059
Atabek <atabekmur@gmail.com>
2018-01-20 00:23:36 +1100

    Add start screen

---
+++ app/src/main/java/com/example/travie/presentation/ui/start/StartActivity.kt
@@ -0,0 +1,31 @@
+package com.example.travie.presentation.ui.start
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.widget.TextView
+import com.example.travie.R
+import kotlinx.android.synthetic.main.activity_start.*
+
+class StartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+
+        startSignIn.setOnClickListener({
+            val intent = Intent(this, StartActivity::class.java)
+            startActivity(intent)
+        })
+
+        val haveAccount = getString(R.string.dont_have_an_account)
+        val signUp = getString(R.string.sign_up)
+        val spannable = SpannableString(haveAccount)
+        spannable.setSpan(ForegroundColorSpan(ContextCompat.getColor(this, R.color.colorPrimary)), haveAccount.length - signUp.length, haveAccount.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        dontHaveAccount.setText(spannable, TextView.BufferType.SPANNABLE)
+    }
+}
